c5c8ccb674045305c799bedefb78ee157bb308e0
Tin Tran <tinhardware@gmail.com>
2022-06-19 16:00:41 +0700

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Tin Tran on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
