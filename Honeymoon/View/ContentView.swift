b5d17c3314b07e0538afa51b58341f35497a3f1f
Tin Tran <tinhardware@gmail.com>
2022-06-19 20:16:51 +0700

    Add list of card views

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -13,16 +13,40 @@ struct ContentView: View {
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
     
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        for honeymoon in honeymoonData {
+            views.append(CardView(honeymoon: honeymoon))
+        }
+        return views;
+    }()
+    
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: {$0.id == cardView.id}) else {
+            return false
+        }
+        return index == 0
+    }
+    
     // MARK: - BODY
     var body: some View {
         VStack {
+            // MARK: - HEADER
             HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
             
             Spacer()
             
-            CardView(honeymoon: honeymoonData[2])
-                .padding()
+            ZStack {
+                ForEach(cardViews) { cardView in
+                    cardView
+                        .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                }
+            }
+            .padding(.horizontal)
             
+            Spacer()
+            
+            // MARK: - FOOTER
             FooterView(showBookingAlert: $showAlert)
         }
         .alert(isPresented: $showAlert) {
